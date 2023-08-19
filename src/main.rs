e23a1597d88e453e87bda1e3b27082817cf37666
JellyGamez <alexcroitoriu2005@gmail.com>
2023-08-19 03:22:05 +0300

    plain backtracking

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::time::Instant;
 struct Sudoku
 {
     board: [[i32; 10]; 10],
@@ -11,22 +12,33 @@ impl Sudoku
     pub fn new(board: [[i32; 10]; 10]) -> Sudoku 
     {
         let mut empty: Vec<(usize, usize)> = Vec::new();
-        for row in 0..10
+        let mut used_row = [[false; 10]; 10];
+        let mut used_col = [[false; 10]; 10];
+        let mut used_box = [[false; 10]; 10];
+        for row in 1..10
         {
-            for col in 0..10
+            for col in 1..10
             {
+                let digit = board[row][col];
+                let ind = (row - 1) / 3 * 3 + (col - 1) / 3 + 1;
                 if board[row][col] == 0
                 {
                     empty.push((row, col));
+                }
+                else 
+                {
+                    used_row[row][digit as usize] = true;
+                    used_col[col][digit as usize] = true;
+                    used_box[ind][digit as usize] = true;
                 }
             }
         }
         Sudoku 
         {
             board: board,
-            used_row: [[false; 10];10],
-            used_col: [[false; 10];10],
-            used_box: [[false; 10];10],
+            used_row: used_row,
+            used_col: used_col,
+            used_box: used_box,
             empty: empty
         }
     }
@@ -43,7 +55,7 @@ impl Sudoku
                 if pos == self.empty.len() - 1
                 {
                     self.print();
-                    return;
+                    std::process::exit(0);
                 }
                 self.used_row[row][digit] = true;
                 self.used_col[col][digit] = true;
@@ -68,7 +80,15 @@ impl Sudoku
     }
 }
 fn main() {
+    let mut line = String::new();
+    std::io::stdin().read_line(&mut line).unwrap();
     let mut board = [[0; 10]; 10];
+    for (i, digit) in line.trim().bytes().enumerate()
+    {
+        board[i / 9 + 1][i % 9 + 1] = digit as i32 - '0' as i32;
+    }
+    let start = Instant::now();
     let mut sudoku: Sudoku = Sudoku::new(board);
+    println!("Time elapsed: {:?}", start.elapsed());
     sudoku.bkt(0);
 }  
