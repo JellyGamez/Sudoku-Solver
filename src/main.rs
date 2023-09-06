d7d31796cf2e24276b25d6e997ba084ebf4ce075
JellyGamez <alexcroitoriu2005@gmail.com>
2023-09-06 22:22:19 +0300

    created config file for further adaptability

--- src/main.rs
+++ src/main.rs
@@ -4,25 +4,30 @@ use std::fs::File;
 
 mod sudoku;
 use crate::sudoku::Sudoku;
+mod config;
+use crate::config::
+{
+    FILENAME as filename, 
+    GRID_LIMIT as grid_limit, 
+    PRINT_STATS as print_stats,
+    PRINT_SOLVED_GRIDS as print_solved_grids,
+    FEWEST_CANDIDATES_HEURISTIC as heuristic
+};
 
 fn main() 
 {
     let mut solved = 0;
     let mut board = [0; 81];
 
-    let filename = "./datasets/easy.txt";
-    let limit = 1000000;
-
     let start = Instant::now();
 
     if let Ok(file) = File::open(filename)
     {
         let lines = io::BufReader::new(file).lines();
-        let lines = lines;
 
         for (i, line) in lines.enumerate()
         {
-            if i == limit
+            if i == grid_limit
             {
                 break;
             }
@@ -32,22 +37,32 @@ fn main()
                 {
                     board[i] = digit as u32 - '0' as u32;
                 }
-                let mut sudoku: Sudoku = Sudoku::new(board, true);
+
+                let mut sudoku: Sudoku = Sudoku::new(board, heuristic);
                 sudoku.solve(0);
-                //println!("{}", sudoku);
+
                 if sudoku.solved
                 {
                     solved += 1;
                 }
+
+                if print_solved_grids
+                {
+                    println!("{}", sudoku);
+                }
+
             }
         }
 
         let elapsed = start.elapsed().as_secs_f32();
 
-        println!("Grids solved: {}/{limit}", solved);
-        println!("Time elapsed: {} seconds", elapsed);
-        println!("Average time per grid: {} seconds", elapsed / limit as f32);
-        println!("Average grids per second: {}", limit as f32 / elapsed);
+        if print_stats
+        {
+            println!("Grids solved: {}/{grid_limit}", solved);
+            println!("Time elapsed: {} seconds", elapsed);
+            println!("Average time per grid: {} seconds", elapsed / grid_limit as f32);
+            println!("Average grids per second: {}", grid_limit as f32 / elapsed);
+        }
     }
     else 
     {
