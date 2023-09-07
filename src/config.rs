6a50e8ffdaebd6bc91218874aae2166f82ae21dd
JellyGamez <alexcroitoriu2005@gmail.com>
2023-09-07 16:25:29 +0300

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,9 @@
-pub const FILE_NAME: &str = "";
-pub const PRINT_STATS: bool = true;
-pub const NUMBER_OF_GRIDS: usize = 1;
-pub const PRINT_SOLVED_GRIDS: bool = false;
-pub const MRV_HEURISTIC: bool = false;
+pub const FILE_NAME: &str = ""; // Input file path
+
+pub const PRINT_STATS: bool = true; // Option to print statistics after solving all grids
+
+pub const NUMBER_OF_GRIDS: usize = 1; // The number of grids to solve from the file
+
+pub const PRINT_SOLVED_GRIDS: bool = false; // Option to print solved grids to standard output
+
+pub const MRV_HEURISTIC: bool = false; // Option to enable fewer candidates first heuristic
